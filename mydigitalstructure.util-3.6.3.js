599f75425eb3cf150b3d1004bdafa09851338826
Mark Byers <mark. byers@ibcom.biz>
2019-04-29 11:07:21 +1000

    Update mydigitalstructure.util-3.6.3.js

--- mydigitalstructure.util-3.6.3.js
+++ mydigitalstructure.util-3.6.3.js
@@ -1261,8 +1261,8 @@ if (typeof $.fn.carousel == 'function')
 
 if (typeof $.fn.dropdown == 'function')
 { 
-    $(document).off('show.bs.dropdown')
-    .on('show.bs.dropdown', function (event)
+    $(document).off('show.bs.dropdown', 'myds-dropdown')
+    .on('show.bs.dropdown', 'myds-dropdown', function (event)
 	{
 		if (event.relatedTarget != undefined)
 		{
