5f9c972be433867df16aab104d1a60dd6b32cac8
ibCom <support@ibcom.biz>
2020-09-25 14:47:26 +1000

    logon totp

--- mydigitalstructure.util-4.0.7.js
+++ mydigitalstructure.util-4.0.7.js
@@ -7686,6 +7686,80 @@ mydigitalstructure._util.factory.core = function (param)
 		}
 	});
 
+	mydigitalstructure._util.controller.add(
+	[
+		{
+			name: 'util-local-cache-save',
+			code: function (param, response)
+			{
+				var key = mydigitalstructure._util.param.get(param, 'key').value;
+				var persist = mydigitalstructure._util.param.get(param, 'persist', {"default": false}).value;
+				var storage = (persist?localStorage:sessionStorage);
+				var data = mydigitalstructure._util.param.get(param, 'data').value;
+
+				if (typeof data !== 'string')
+				{
+					data = JSON.stringify(data);
+				}
+
+				storage.setItem(key, data);
+
+				mydigitalstructure._util.onComplete(param);
+			}
+		},
+		{
+			name: 'util-local-cache-search',
+			code: function (param, response)
+			{
+				var key = mydigitalstructure._util.param.get(param, 'key').value;
+				var persist = mydigitalstructure._util.param.get(param, 'persist', {"default": false}).value;
+				var storage = (persist?localStorage:sessionStorage);
+				var isJSON = mydigitalstructure._util.param.get(param, 'isJSON', {"default": key.toLowerCase().indexOf('.json') != -1}).value;
+				var onComplete = mydigitalstructure._util.param.get(param, 'onComplete').value;
+
+				var data = storage.getItem(key);
+
+				if (data == null) {data = undefined}
+
+				if (isJSON && data !== undefined)
+				{
+					data = JSON.parse(data);
+				}
+
+				if (onComplete != undefined)
+				{
+					param = mydigitalstructure._util.param.set(param, 'data', data);
+					mydigitalstructure._util.onComplete(param);
+				}
+				else
+				{
+					return data;
+				}
+			}
+		},
+		{
+			name: 'util-local-cache-remove',
+			code: function (param, response)
+			{
+				var key = mydigitalstructure._util.param.get(param, 'key').value;
+				var persist = mydigitalstructure._util.param.get(param, 'persist', {"default": false}).value;
+				var storage = (persist?localStorage:sessionStorage);
+				var all = mydigitalstructure._util.param.get(param, 'all', {"default": false}).value;
+
+				if (all)
+				{
+					storage.clear()
+				}
+				else
+				{
+					storage.removeItem(key);
+				}	
+
+				mydigitalstructure._util.onComplete(param);
+			}
+		}
+	]);
+
 	app._util = mydigitalstructure._util;
 	app.invoke = mydigitalstructure._util.controller.invoke;
 	app.add = mydigitalstructure._util.controller.add;
